7a084ea949aac264fe6f5d0b03bf6143052f463b
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-16 16:50:21 +0530

    Completed project

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ var textinput = document.querySelector("#txt-input");
 var textoutput=document.querySelector("#output");
 var btnTranslate=document.querySelector("#btn-translate");
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    output.innerText=("banana"+textinput.value);
-})
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslatedURL(input){
+    return serverURL + "?"+"text=" +input
+}
+
+function errorhandler(error){
+    console.log("error occured",error);
+}
+
+function clickEventHandler(){
+    var inputtext = textinput.value; //Getting input
+
+    fetch(getTranslatedURL(inputtext))
+    .then(response=>response.json())
+    .then(json=>{
+      var translatedtext =  json.contents.translated;
+      textoutput.innerText = translatedtext;
+    })
+    .catch(errorhandler)
+};
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
 
 
 
