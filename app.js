23c4ccd009b0b96935cf4f856e45d5545853a7a4
sivasubramanian11 <Sivaifs4@gmail.com>
2022-08-16 15:04:08 +0530

    Input output completed

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var textoutput=document.querySelector("#output");
 var btnTranslate=document.querySelector("#btn-translate");
 
 btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked");
-    console.log(textinput.value);
+    output.innerText=("banana"+textinput.value);
 })
+
+
 
